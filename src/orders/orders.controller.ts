f9a2fc45fd3a83b0f948f1a06032920232be96ef
t1fmarin <fmarin@iuvity.com>
2024-08-24 17:02:22 -0500

    feat: add implementation for findByStatus and changeStatus

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -7,12 +7,15 @@ import {
   Inject,
   ParseUUIDPipe,
   Query,
+  Patch,
 } from '@nestjs/common';
 
 import { ORDER_SERVICE } from '../config/injection-tokens';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { OrderPaginationDto } from '../common/dtos/order-pagination.dto';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { StatusDto } from 'src/common/dtos/status.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -39,12 +42,42 @@ export class OrdersController {
     );
   }
 
-  @Get(':id')
+  @Get('id/:id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.ordersService.send('findOneOrder', { id }).pipe(
       catchError((error) => {
         throw new RpcException(error);
       }),
     );
+  }
+
+  @Get(':status')
+  async findAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+      return this.ordersService.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    try {
+      return this.ordersService.send('changeOrderStatus', {
+        id,
+        status: statusDto.status,
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 }
